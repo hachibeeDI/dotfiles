8d8768cb64c9fbfbafa065f3826d0b8846a88f7c
OGURA_Daiki <8hachibee125@gmail.com>
2012-08-11 22:12:54 +0900

    snipet templates for python

---
+++ .vim/templates/base-main.cs
@@ -0,0 +1,21 @@
+/* 
+ * {{_name_}}
+ *
+ * Written By: OGURA_Daiki
+ *
+ */
+
+namespace {{_expr_:getcwd()}}{
+    using System;
+    using System.Windows.Forms;
+
+    class {{_name_}} 
+    {
+        public {{_name_}}({{_cursor_}})
+        {
+
+        }
+
+    }
+
+}
