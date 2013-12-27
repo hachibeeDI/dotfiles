735f39462a945f27b21e17d06b260bf8a7d848e1
dogura <daiki.ogura@soliton.co.jp>
2013-12-27 16:50:47 +0900

    add osx setup script

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@
 source "https://rubygems.org"
 
 gem "brewdler"
+gem "rake"
--- setup/osx/init.sh
+++ setup/osx/init.sh
@@ -1,49 +1,9 @@
 #!/bin/sh
 
-# homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-
-# must
-brew install python --with-brewed-openssl
-brew install mercurial
-
-brew install readline
-
-brew install --disable-etcdir zsh
-brew install zsh-completions
-
-echo 'if you want to use zsh installed from homebrew, have to append its path on /etc/shells.'
-echo ''
-
-brew install vim --override-system-vi --with-lua --with-ruby --with-python --with-python3
-
-for pkg in "automake" "autoconf" "openssl" "sqlite"
-do
-    brew install $pkg
-done
-
-# commons
-for pkg in "git" "coreutils" "gnu-sed" "wget" "gawk" "boost"
-do
-    brew install $pkg
-done
-
-# utils
-for pkg in "reattach-to-user-namespace" "tig" "source-highlight" "colordiff" "autojump"
-do
-    brew install $pkg
-done
-
-for pkg in "tmux" "ctags" "the_silver_searcher" "sl"
-do
-    brew install $pkg
-done
-
-brew install ruby --with-doc
-brew install ruby-build
-
-mkdir ~/Applications
-brew linkapps
-
-sl
-
+if [ ! -x "$(which brew)" ]; then
+    # Install homebrew
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+    echo "Homebrew is already setuped."
+    exit 1
+fi;
